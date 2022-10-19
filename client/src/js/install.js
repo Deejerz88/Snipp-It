c659e952de30f1c4586e810434ac62684262f096
DJ <djmcmillan93@gmail.com>
2022-10-19 16:01:45 -0400

    install deferred prompt

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,17 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
-  event.preventDefault()
+  window.deferredPrompt = event
   butInstall.style.visibility = 'visible';
 
 });
 
 butInstall.addEventListener('click', async () => {
-  window.prompt()
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
+
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
   butInstall.style.visibility = 'hidden';
   butInstall.setAttribute('disabled', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
   console.log('App installed');
+  window.deferredPrompt = null;
 });
